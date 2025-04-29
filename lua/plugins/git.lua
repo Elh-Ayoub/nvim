644dc7c0beaf324246e209dc466fa537bae61dfe
Ayoub El Haddadi <ayoub1998elh@gmail.com>
2025-04-29 16:16:12 +0100

    Added plugins for undotree and git

---
+++ lua/plugins/git.lua
@@ -0,0 +1,57 @@
+return {
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({
+				preview_config = {
+					border = "rounded",
+				},
+			})
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "Preview Git Changes" })
+			vim.keymap.set(
+				"n",
+				"<leader>gt",
+				":Gitsigns toggle_current_line_blame<CR>",
+				{ desc = "Toggle current line git blame" }
+			)
+		end,
+	},
+	{
+		"tpope/vim-fugitive",
+		config = function()
+			vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Open git status with - fugitive" })
+			vim.keymap.set("n", "<leader>gb", ":Git blame<CR>", { desc = "Open git Blame - fugitive" })
+
+			local aelhaddadi_fugitive = vim.api.nvim_create_augroup("aelhaddadi_fugitive", {})
+
+			local autocmd = vim.api.nvim_create_autocmd
+			autocmd("BufWinEnter", {
+				group = aelhaddadi_fugitive,
+				pattern = "*",
+				callback = function()
+					if vim.bo.ft ~= "fugitive" then
+						return
+					end
+
+					local bufnr = vim.api.nvim_get_current_buf()
+					local opts = { buffer = bufnr, remap = false }
+					vim.keymap.set("n", "<leader>p", function()
+						vim.cmd.Git("push")
+					end, opts)
+
+					-- rebase always
+					vim.keymap.set("n", "<leader>P", function()
+						vim.cmd.Git({ "pull", "--rebase" })
+					end, opts)
+
+					-- NOTE: It allows me to easily set the branch i am pushing and any tracking
+					-- needed if i did not set the branch up correctly
+					vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
+				end,
+			})
+
+			vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
+			vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+		end,
+	},
+}
